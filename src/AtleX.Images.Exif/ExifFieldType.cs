631434a4c572ccf24fa8369e569b9e1fcf5115b0
akamsteeg <alex@atlex.nl>
2015-03-17 09:55:50 +0000

    Added more EXIF fields and added the data type to all fields in a remark

--- src/AtleX.Images.Exif/ExifFieldType.cs
+++ src/AtleX.Images.Exif/ExifFieldType.cs
@@ -13,45 +13,113 @@ namespace AtleX.Images.Exif
         /// <summary>
         /// Image width
         /// </summary>
+        /// <remarks>
+        /// The datatype is Long
+        /// </remarks>
         ImageWidth = 256,
         /// <summary>
         /// Image height
         /// </summary>
+        /// <remarks>
+        /// The datatype is Long
+        /// </remarks>
         ImageHeight = 257,
+        /// <summary>
+        /// The number of bits per image component
+        /// </summary>
+        /// <remarks>
+        /// The datatype is Short
+        /// </remarks>
+        BitsPerSample = 258,
+        /// <summary>
+        /// The pixel composition
+        /// </summary>
+        /// <remarks>
+        /// The datatype is Short
+        /// </remarks>
+        PhotometricInterpretation = 262,
 
         /// <summary>
         /// Image title
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         ImageDescription = 270,
         /// <summary>
         /// Camera maker
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         CameraMake = 271,
         /// <summary>
         /// Camera model
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         CameraModel = 272,
 
         /// <summary>
+        /// The image orientation
+        /// </summary>
+        /// <remarks>
+        /// The datatype is Short
+        /// </remarks>
+        Orientation = 274,
+        /// <summary>
+        /// The number of components per pixel
+        /// </summary>
+        /// <remarks>
+        /// The datatype is Short
+        /// </remarks>
+        SamplesPerPixel = 277,
+        /// <summary>
         ///  Unit of X and Y resolution
         /// </summary>
+        /// <remarks>
+        /// The datatype is Short
+        /// </remarks>
         ResolutionUnit = 296,
 
         /// <summary>
         /// The editing software used
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         SoftwareUsed = 305,
         /// <summary>
         /// Capture date/time
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         DateTime = 306,
         /// <summary>
         /// Photographer
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         Artist = 315,
+
+        /// <summary>
+        /// The position of chrominance components in relation to 
+        /// the luminance component
+        /// </summary>
+        /// <remarks>
+        /// The datatype is Short
+        /// </remarks>
+        YCbCrPositioning = 531,
+
         /// <summary>
         /// Copyright
         /// </summary>
+        /// <remarks>
+        /// The datatype is ASCII
+        /// </remarks>
         Copyright = 33432,
     }
 }
